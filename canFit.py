9638c04daa8fba8012b135e4894c5028935e0093
hlmaestretti <huntermaestretti@gmail.com>
2023-07-08 08:02:35 -0700

    Added essential parts to canFit.py and added minor testing

--- canFit.py
+++ canFit.py
@@ -26,31 +26,19 @@ def board_sort(list_of_boards):
             pos -= 1
         list_of_boards[pos + 1] = board
 
-def larger_number(num1, num2):
-    """
-    larger_number is a simple compare function that returns the larger of two numbers. It returns the second
-    number if they are the same size.
-    """
-    if num1 > num2:
-        return num1
-
-    return num2
     
-def can_cut_boards_from_boards(list_of_cuts, boards, saw_thickness, cuts = ""):
+def canFit(list_of_cuts, boards, saw_thickness=.125, cuts=""):
     """
-    This will act recursively to find the see if all the list of cuts can be done on the given boards.
+    The canFit function tests to see if a list of cutouts can fit into the available boards recursively.
 
-    First set cuts up to be a list and maybe organize list
-
-    see if first cut fits on one of the boards.
-        if it does, then cut that board into 3 pieces (the desired cut and the 2 scraps). when calling itself, add the
-        variations of the scrap and delete the board that was used. This gives two recursive calls
-
-        if it doesn't fit the first, continue to the next
-
-        if it doesn't fit any, then return false
-
-    end recursion when either the list_of_cuts is empty or a piece is found that cannot fit onto any board, return false
+    Receives:
+        - list_of_cuts  - Array of Board objects that indicate each cut to be done
+        - boards        - Array of Board objects that indicates the available wood we have
+        - saw_thickness - Default parameter that represents the thickness of the saw blade in inches
+        - cuts          - parameter used during the recursive steps
+    Returns:
+        - If the cuts can fit, it will list out the relative positions of where each cut should be
+        - IF they don't fit, it will return False
     """
     
     if cuts == "":
@@ -68,22 +56,22 @@ def can_cut_boards_from_boards(list_of_cuts, boards, saw_thickness, cuts = ""):
             possible_actions = []
             # possible cuts that could be done on the board
             if piece.get_width() >= cut.get_width() and piece.get_length() >= cut.get_length():
-                scraps1 = [Board(cut.get_length(), piece.get_width() - cut.get_width()),
-                           Board(piece.get_width(), piece.get_length() - cut.get_length())
+                scraps1 = [Board(cut.get_length(), piece.get_width() - cut.get_width(), cut.thickness),
+                           Board(piece.get_width(), piece.get_length() - cut.get_length(), cut.thickness)
                            ]
-                scraps2 = [Board(piece.get_width() - cut.get_width(), piece.get_length()),
-                           Board(cut.get_width(), piece.get_length() - cut.get_length())
+                scraps2 = [Board(piece.get_width() - cut.get_width(), piece.get_length(), cut.thickness),
+                           Board(cut.get_width(), piece.get_length() - cut.get_length(), cut.thickness)
                            ]
                 possible_cuts.append(scraps1)
                 possible_cuts.append(scraps2)
                 possible_actions.append("Horizontal")
                 possible_actions.append("Horizontal")
             if piece.get_width() >= cut.get_length() and piece.get_length() >= cut.get_width():
-                scraps3 = [Board(piece.get_width() - cut.get_length(), cut.get_width()),
-                           Board(piece.get_length() - cut.get_width(), piece.get_length())
+                scraps3 = [Board(piece.get_width() - cut.get_length(), cut.get_width(), cut.thickness),
+                           Board(piece.get_length() - cut.get_width(), piece.get_length(), cut.thickness)
                            ]
-                scraps4 = [Board(piece.get_width() - cut.get_length(), piece.get_length()),
-                           Board(piece.get_length() - cut.get_width(), cut.get_length())
+                scraps4 = [Board(piece.get_width() - cut.get_length(), piece.get_length(), cut.thickness),
+                           Board(piece.get_length() - cut.get_width(), cut.get_length(), cut.thickness)
                            ]
                 possible_cuts.append(scraps3)
                 possible_cuts.append(scraps4)
@@ -94,8 +82,7 @@ def can_cut_boards_from_boards(list_of_cuts, boards, saw_thickness, cuts = ""):
                 temp = boards[:]
                 boards.pop(index)
                 boards = boards + possible_cuts[i]
-                testing_cuts = can_cut_boards_from_boards(list_of_cuts[1:], boards, saw_thickness,
-                                                          cuts + [possible_actions[i]])
+                testing_cuts = canFit(list_of_cuts[1:], boards, saw_thickness, cuts + [possible_actions[i]])
                 boards = temp
                 if testing_cuts is not False:
                     return testing_cuts
@@ -105,13 +92,13 @@ def can_cut_boards_from_boards(list_of_cuts, boards, saw_thickness, cuts = ""):
 
 if __name__ == "__main__":
     print("Please give the dimensions of the boards you want to cut one at a time. Leave blank if you are done."
-          "Format: length,width")
-    list_cuts = [(11.5, 1), (11.5, 1), (11.5, 1), (11.5, 1), (11.5, 1), (11.5, 1), (11.5, 1),
-                 (11.5, 1), (11.5, 1), (11.5, 1), (11.5, 1), (11.5, 1), (11.5, 1), (11.5, 1),
-                 (11.5, 1), (11.5, 1), (11.5, 1), (11.5, 1), (11.5, 1), (11.5, 1), (11.5, 1),
-                 (11.5, 1), (11.5, 1), (11.5, 1), (11.5, 1), (11.5, 1), (11.5, 1), (11.5, 1),
-                 (40, .75)]
-    list_boards = [(48, 9)]
+          "Format: length, width, thickness")
+    list_cuts = [(11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1),
+                 (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1),
+                 (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1),
+                 (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1),
+                 (40, .75, 1)]
+    list_boards = [(48, 9, 1)]
 
     print(list_cuts)
     # while True:
@@ -119,17 +106,17 @@ if __name__ == "__main__":
     #     if item == "":
     #         break
     #     nums = item.split(",")
-    #     list_boards.append((float(nums[0]), float(nums[1])))
+    #     list_boards.append((float(nums[0]), float(nums[1]), float(nums[2]))
 
     for cut_index, cut in enumerate(list_cuts):
         try:
-            list_cuts[cut_index] = Board(cut[0], cut[1])
+            list_cuts[cut_index] = Board(cut[0], cut[1], cut[2])
         except ValueError:
             raise InvalidInput
 
     for board_index, board in enumerate(list_boards):
         try:
-            list_boards[board_index] = Board(board[0], board[1])
+            list_boards[board_index] = Board(board[0], board[1], board[2])
         except ValueError:
             raise InvalidInput
 
@@ -149,7 +136,7 @@ if __name__ == "__main__":
     if not isinstance(saw_blade, (int, float)):
         raise InvalidInput
 
-    test = can_cut_boards_from_boards(list_cuts, list_boards, saw_blade)
+    test = canFit(list_cuts, list_boards, saw_blade)
     if not test:
         print("The pieces you requested cannot all fit on this board!")
 
