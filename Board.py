64c873b696ec059f5b3afd27c8ac2f97f89b25f4
hlmaestretti <huntermaestretti@gmail.com>
2023-07-08 07:23:49 -0700

    Added necessary parts for Board implementation

--- Board.py
+++ Board.py
@@ -18,34 +18,35 @@ class Board:
         - can_fit     - Checks to see if another board can be cut from the board. True if it can, false
                         otherwise
     """
-    def __init__(self, length, width):
+    def __init__(self, length, width, thickness):
         self._length = length
         self._width = width
+        self._thickness = thickness
     
     def get_length(self):
         """
         The get_length method returns the length of the board.
-        
-        Return:
-            length
         """
         return self._length
     
     def get_width(self):
         """
         The get_width method returns the width of the board.
-        
-        Return:
-            Width
         """
         return self._width
+
+    def get_thickness(self):
+        """
+        The get_thickness method returns the thickness of the board.
+        """
+        return self._thickness
     
     def shift_board(self):
         """
         The shift_board method switches the width and length of the board.
         
         Returns:
-            None
+            - None
         """
         temp = self._length
         self._length = self._width
@@ -53,13 +54,36 @@ class Board:
     
     def get_area(self):
         """
-        The get_area returns the area of the board by doing the caluclation length * width.
+        The get_area returns the area of the face of the board by doing the calculation length * width.
         
         Return:
-            Area of the board
+            - Area of the board
         """
         return self._length * self._width
-    
+
+    def get_surface_area(self):
+        """
+        The get_surface_area method calculates the surface area of the whole board. by using the following
+        equation:  SA = 2(length*width) + 2(length*thickness) + 2(width*thickness)
+
+        Return:
+            - Surface Area of the board
+        """
+        l = self._length
+        w = self._width
+        t = self._thickness
+        return 2*(l*w) + 2*(l*t) + 2*(w*t)
+
+    def get_volume(self):
+        """
+        The get_volume method calculates the volume of the board by using the following equation:
+        v = length*width*thickness
+
+        Return:
+            - Volume of the board
+        """
+        return self._length * self._width * self._thickness
+
     def can_fit(self, another_board):
         """
         The can_fit methods checks to see if another board can be cut from this board. It does
@@ -69,12 +93,13 @@ class Board:
             Boolean - True if it is possible
                     - False if it is not
         """
-        if self._length >= another_board.get_length():
-            if self._width >= another_board.get_width():
-                return True
-        
-        if self._width >= another_board.get_length():
-            if self._length >= another_board.get_width():
-                return True
+        if self._thickness <= another_board.get_thickness():
+            if self._length >= another_board.get_length():
+                if self._width >= another_board.get_width():
+                    return True
+
+            if self._width >= another_board.get_length():
+                if self._length >= another_board.get_width():
+                    return True
             
         return False
