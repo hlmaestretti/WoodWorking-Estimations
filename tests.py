c05f9eb5b81277d1e6db893a293cdc85cadacab1
hlmaestretti <huntermaestretti@gmail.com>
2023-07-09 12:49:58 -0700

    Fixed major bug that caused an infinite loop with smaller pieces. Created a better way to understand where the pieces go in relation to eachother. Moved Testing to tests.py

---
+++ tests.py
@@ -0,0 +1,36 @@
+"""
+This file contains various unit tests for the main functionality of the program
+"""
+
+import unittest
+import canFit
+
+class TestVariousFunctions(unittest.TestCase):
+
+    def test_canFit(self):
+        """
+        This tests whether canFit functions properly
+        """
+        list_cuts = [(11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1),
+                     (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1),
+                     (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1),
+                     (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1),
+                     (40, .75, 1)]
+        list_boards = [(48, 9, 1)]
+
+        test = canFit.canFit(list_cuts, list_boards, .125)
+        self.assertIsInstance(test, list, "Test 1 passed")
+
+        list_cuts = [(11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1),
+                     (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1),
+                     (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1),
+                     (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1), (11.5, 1, 1),
+                     (40, .75, 1)]
+        list_boards = [(47, 3, 1)]
+        test = canFit.canFit(list_cuts, list_boards, .125)
+        self.assertFalse(test, "Test 2 passed")
+
+        list_cuts = [(47, 3, 1)]
+        list_boards = [(47, 3, 1)]
+        test = canFit.canFit(list_cuts, list_boards, .125)
+        self.assertIsInstance(test, list, "Test 3 passed")
